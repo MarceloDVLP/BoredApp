d12a0656b57c8db32492e52b194237ade1a0a600
Marcelo Carvalho <marcelodvlp@gmail.com>
2023-06-09 11:35:44 -0300

    Initial Commit

---
+++ BoredApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BoredApp
+//
+//  Created by Marcelo Carvalho on 09/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
