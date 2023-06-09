8ff75841f660fbb013ce6804da7f15282933f3e9
Marcelo Carvalho <marcelodvlp@gmail.com>
2023-06-09 11:42:24 -0300

    change default ViewController file to BoredListViewControler

--- BoredApp/BoredListViewControler.swift
+++ BoredApp/BoredListViewControler.swift
@@ -7,13 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class BoredListViewControler: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
 
